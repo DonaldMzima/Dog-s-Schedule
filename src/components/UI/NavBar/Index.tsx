3eb8d792c33e3fc9cff51febe130d762df85a2b9
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-01-20 16:21:21 +0200

    ref:fixing/home/page

--- src/components/UI/NavBar/Index.tsx
+++ src/components/UI/NavBar/Index.tsx
@@ -1,52 +1,66 @@
-import { Avatar, Box, Button, Flex, Link, Stack } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Link,
+  Stack,
+  Center,
+} from '@chakra-ui/react'
+import { useMediaQuery } from '@chakra-ui/react'
 import { FaCalendar, FaHome } from 'react-icons/fa'
 
 const NavBar = () => {
   return (
     <>
-      <Box bg={'#709c84'} height={90} pos="fixed" width="100%" px={4}>
+      <Box bg={'#f8f9eb'} height={90} pos="fixed" width="100%" px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
-              <Link href="/">
-                <Box height="14px" width="400px">
-                  <Button
-                    size="md"
-                    height="48px"
-                    width="100px"
-                    bg="green.400"
-                    _hover={{ border: '2px solid #709c84' }}
-                  >
-                    Home
-                  </Button>
-                </Box>
-              </Link>
-              <Link href="/calender">
-                <Box height="14px" width="400px">
-                  <Button
-                    size="md"
-                    height="48px"
-                    width="100px"
-                    bg="green.400"
-                    _hover={{ border: '2px solid #709c84' }}
-                  >
-                    Calender
-                  </Button>
-                </Box>
-              </Link>
-              <Link href="/calender">
-                <Box height="14px" width="400px">
-                  <Button
-                    size="md"
-                    height="48px"
-                    width="100px"
-                    bg="green.400"
-                    _hover={{ border: '2px solid #709c84' }}
-                  >
-                    Settings
-                  </Button>
-                </Box>
-              </Link>
+            <Stack direction={'row'}>
+              <Center textAlign={'center'} width="1900px">
+                <Link href="/">
+                  <Box height="14px" width="400px">
+                    <Button
+                      size="md"
+                      height="48px"
+                      width="100px"
+                      color="white"
+                      bg="#1f1f1a"
+                      _hover={{ border: '2px solid white' }}
+                    >
+                      Home
+                    </Button>
+                  </Box>
+                </Link>
+                <Link href="/calender">
+                  <Box height="14px" width="400px">
+                    <Button
+                      size="md"
+                      height="48px"
+                      width="100px"
+                      color="white"
+                      bg="#1f1f1a"
+                      _hover={{ border: '2px solid white' }}
+                    >
+                      Calender
+                    </Button>
+                  </Box>
+                </Link>
+                <Link href="/calender">
+                  <Box height="14px" width="400px">
+                    <Button
+                      size="md"
+                      height="48px"
+                      width="100px"
+                      color="white"
+                      bg="#1f1f1a"
+                      _hover={{ border: '2px solid white' }}
+                    >
+                      Settings
+                    </Button>
+                  </Box>
+                </Link>
+              </Center>
             </Stack>
           </Flex>
         </Flex>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import HomePage from '@/components/UI/Views/HomeScreen/Index'
 import Nav from '@/components/UI/NavBar/Index'
+import { Box } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
