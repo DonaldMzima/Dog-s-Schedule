1d2f585b13b096c206468e4055fac833a097d60e
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-01-25 23:35:07 +0200

    ref: add conditional rendering

--- src/components/UI/NavBar/Index.tsx
+++ src/components/UI/NavBar/Index.tsx
@@ -1,85 +1,93 @@
 import {
-  Avatar,
+  Text,
   Box,
   Button,
   Flex,
   Link,
   Stack,
   Center,
+  useMediaQuery,
 } from '@chakra-ui/react'
-import { useMediaQuery } from '@chakra-ui/react'
-import { FaCalendar, FaHome } from 'react-icons/fa'
+
+import { RiHome4Fill } from 'react-icons/Ri'
 
 const NavBar = () => {
-  const [isLargerThanHD, isDisplayingInBrowser] = useMediaQuery([
-    '(min-width: 360px)',
-    '(display-mode: browser)',
-  ])
-
-  function determineText() {
-    if (isLargerThanHD) {
-      return `high resolution you got there ${
-        isDisplayingInBrowser ? 'in your browser' : 'on your screen'
-      }`
-    }
-
-    return isDisplayingInBrowser
-      ? 'rendering in a browser'
-      : 'rendering on something else, e.g. PWA'
-  }
-
+  const [isMobile] = useMediaQuery('(max-width: 768px)')
   return (
     <>
       <Box bg={'#f8f9eb'} height={90} pos="fixed" width="100%" px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'}>
-              <Center width="1690px">
-                <Link href="/">
-                  <Box height="14px" width="400px">
-                    <Button
-                      size="md"
-                      height="48px"
-                      width="100px"
-                      color="white"
-                      bg="#1f1f1a"
-                      _hover={{ border: '2px solid white' }}
-                    >
-                      Home
-                    </Button>
-                  </Box>
-                </Link>
-                <Link href="/calender">
-                  <Box height="14px" width="400px">
-                    <Button
-                      size="md"
-                      height="48px"
-                      width="100px"
-                      color="white"
-                      bg="#1f1f1a"
-                      _hover={{ border: '2px solid white' }}
-                    >
-                      Calender
-                    </Button>
-                  </Box>
-                </Link>
-                <Link href="/settings">
-                  <Box height="14px" width="400px">
-                    <Button
-                      size="md"
-                      height="48px"
-                      width="100px"
-                      color="white"
-                      bg="#1f1f1a"
-                      _hover={{ border: '2px solid white' }}
-                    >
-                      Settings
-                    </Button>
-                  </Box>
-                </Link>
-              </Center>
-            </Stack>
-          </Flex>
+          >
+          <Stack direction={'row'}>
+            <Center width="1690px">
+              <Link href="/">
+                <Box height="14px" width="400px">
+                  {isMobile ? (
+                    <>
+                      <RiHome4Fill />
+                    </>
+                  ) : (
+                    <>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="100px"
+                        color="white"
+                        bg={'#1f1f1a'}
+                        _hover={{ border: '2px solid white' }}
+                      >
+                        Home
+                      </Button>
+                    </>
+                  )}
+                </Box>
+              </Link>
+              <Link href="/calender">
+                <Box height="14px" width="400px">
+                  {isMobile ? (
+                    <>
+                      <RiHome4Fill />
+                    </>
+                  ) : (
+                    <>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="100px"
+                        color="white"
+                        bg="#1f1f1a"
+                        _hover={{ border: '2px solid white' }}
+                      >
+                        Calender
+                      </Button>
+                    </>
+                  )}
+                </Box>
+              </Link>
+              <Link href="/settings">
+                <Box height="14px" width="400px">
+                  {isMobile ? (
+                    <>
+                      <RiHome4Fill />
+                    </>
+                  ) : (
+                    <>
+                      <Button
+                        size="md"
+                        height="48px"
+                        width="100px"
+                        color="white"
+                        bg="#1f1f1a"
+                        _hover={{ border: '2px solid white' }}
+                      >
+                        Settings
+                      </Button>
+                    </>
+                  )}
+                </Box>
+              </Link>
+            </Center>
+          </Stack>
         </Flex>
       </Box>
     </>
