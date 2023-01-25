5d432c68002a6aab637fd324d8442f8ff46f1e9e
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-01-25 23:39:39 +0200

    fix: fix navbar

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -5,32 +5,34 @@ import Nav from '@/components/UI/NavBar/Index'
 
 const settings = () => {
   return (
-    <Container
-      maxW={'9xl'}
-      height="999px"
-      bgGradient={[
-        'linear(to-tr, white, white)',
-        'linear(to-t, white, white)',
-
-        'linear(to-b, white, yellow)',
-      ]}
-    >
+    <>
       <Nav />
-      <Stack
-        as={Box}
-        textAlign={'center'}
-        spacing={{ base: 8, md: 14 }}
-        py={{ base: 20, md: 36 }}
+      <Container
+        maxW={'9xl'}
+        height="999px"
+        bgGradient={[
+          'linear(to-tr, white, white)',
+          'linear(to-t, white, white)',
+
+          'linear(to-b, white, yellow)',
+        ]}
       >
-        <Text>
-          Contant <br />
-          Coming soon...
-        </Text>
-        <Center textAlign={'center'}>
-          <SvgComponent />
-        </Center>
-      </Stack>
-    </Container>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Text>
+            Contant <br />
+            Coming soon...
+          </Text>
+          <Center textAlign={'center'}>
+            <SvgComponent />
+          </Center>
+        </Stack>
+      </Container>
+    </>
   )
 }
 export default settings
