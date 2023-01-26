01220ba484c6e61ec3636c35a2633c46ca01b4f3
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-01-26 14:28:26 +0200

    fix:delete button component

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -1,19 +1,13 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react'
-import {
-  Stack,
-  Box,
-  Tooltip,
-  Container,
-  Center,
-  useMediaQuery,
-} from '@chakra-ui/react'
-import AddButton from '@/components/Modals/AddButton/Index'
+import { Stack, Box, Container, Center, useMediaQuery } from '@chakra-ui/react'
 import { useRecoilValue } from 'recoil'
 import Nav from '@/components/UI/NavBar/Index'
 import CalenderBody from '@/components/CalenderSection/Calender/Index'
 import NavigationBar from '@/components/UI/2ndNavBar/Index'
+import FloatingButtons from '@/components/UI/FloatingButtons/Index'
 import { schedulesState } from '@/store/atoms'
+import SvgComponent from '@/components/Svg/HomeSvg'
 
 const calender = () => {
   const dogSchedules = useRecoilValue(schedulesState)
@@ -27,28 +21,24 @@ const calender = () => {
         minHeight="100vh"
         bgGradient={['linear(to-b, white, yellow)']}
       >
-        <Center width="2520px" height="250px">
-          <Tooltip hasArrow label="Add Schedules" bg="#FFFF00">
-            <Box pos="fixed">
-              <AddButton />
-            </Box>
-          </Tooltip>
-        </Center>
+        <FloatingButtons />
 
         <div>
           {dogSchedules?.length > 0 ? (
             dogSchedules?.map((walkSchedules: any) => {
               return (
                 <Center key={walkSchedules.Schedule}>
+                  <SvgComponent width={100} />
                   <Box
-                    bg="#980c84"
-                    border="1px"
+                    bg="#1f1f1a"
+                    borderTopRadius="md"
                     borderColor="gray.200"
                     h={200}
-                    w={400}
+                    w={200}
                   >
                     <Stack
                       textAlign={'center'}
+                      color="white"
                       align={'center'}
                       spacing={{ base: 8, md: 10 }}
                       py={{ base: 20, md: 28 }}
@@ -71,7 +61,7 @@ const calender = () => {
           )}
         </div>
       </Container>
-      <Box>{isMobile && <NavigationBar />}</Box>
+      <div>{isMobile && <NavigationBar />}</div>
     </>
   )
 }
