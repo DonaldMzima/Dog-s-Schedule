efed8612bd518f83aa902837ab68361c30c25e3a
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-05-25 15:53:05 +0200

    ref:remove MyCalender component and fix the format

--- client/src/pages/calender.tsx
+++ client/src/pages/calender.tsx
@@ -1,10 +1,12 @@
 import SecondMyCalender from '@/components/CalenderSection/2ndMyCalender'
 import { Box, useMediaQuery } from '@chakra-ui/react'
-import MyCalender from '@/components/CalenderSection/MyCalender'
 
 const Calender = () => {
-  
-  return <Box> <SecondMyCalender /></Box>
+  return (
+    <Box>
+      <SecondMyCalender />
+    </Box>
+  )
 }
 
 export default Calender
