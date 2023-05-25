60b619a8252a71dd3a364e835c0c4d86e7ddcb3b
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-05-25 15:49:20 +0200

    ref:add with for the search component

--- client/src/components/CalenderSection/SearchFilter/Index.tsx
+++ client/src/components/CalenderSection/SearchFilter/Index.tsx
@@ -1,16 +1,24 @@
 /* eslint-disable react/no-children-prop */
-import { Input, InputGroup, InputLeftElement, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+} from '@chakra-ui/react'
 import React, { useState } from 'react'
 import ReactDom from 'react-dom'
 import { GrSearchAdvanced } from 'react-icons/gr'
 
 function Search() {
   return (
-    <Stack size={5}>
-      <InputGroup>
-        <InputLeftElement children={<GrSearchAdvanced size={'1em'} />} />
-        <Input type="text" htmlSize={4} width="auto" />
-      </InputGroup>
+    <Stack>
+      <Box width={5}>
+        <InputGroup>
+          <InputLeftElement children={<GrSearchAdvanced size={'1em'} />} />
+          <Input type="text" htmlSize={4} width="auto" />
+        </InputGroup>
+      </Box>
     </Stack>
   )
 }
