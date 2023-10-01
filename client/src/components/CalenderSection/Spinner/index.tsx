06a3e2c5fbbc052add9dff1c29aff1565e7fd138
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-10-01 15:24:58 +0200

    ref:add the opacity

--- client/src/components/CalenderSection/Spinner/index.tsx
+++ client/src/components/CalenderSection/Spinner/index.tsx
@@ -10,7 +10,7 @@ const CustomSpinner = ({ text }: CustomSpinnerType) => {
         align={'center'}
         spacing={{ base: 8, md: 10 }}
         py={{ base: 20, md: 28 }}
-        bg={'white'}
+        bg={'rgba(255, 255, 255, 0.8)'}
       >
         <Spinner
           thickness="4px"
