1f3253815f1d8fd083ba1aa6e4fef075d60e2329
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-10-05 15:27:54 +0200

    ref:comment out apollo client file

---
+++ client/apolloClient.ts
@@ -0,0 +1,14 @@
+// // import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+// import ApolloClient from 'apollo-boost'
+// // const httpLink = createHttpLink({
+// //   uri: 'https://swfkedcclgqaeusybpxy.graphql.eu-central-1.nhost.run/v1',
+// // })
+
+// export const client = new ApolloClient({
+//   uri: 'https://swfkedcclgqaeusybpxy.graphql.eu-central-1.nhost.run/v1',
+// })
+
+// // export const client = new ApolloClient({
+// //   link: httpLink,
+// //   // cache: new InMemoryCache(),
+// // })
