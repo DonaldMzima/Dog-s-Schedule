9ae8bc0f8514e64989d06c33f51f8d2430f67c09
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-10-17 16:21:06 +0200

    ref:fix navbar

--- client/src/components/UI/NavBar/Index.tsx
+++ client/src/components/UI/NavBar/Index.tsx
@@ -1,99 +1,61 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Link,
-  Stack,
-  Center,
-  useMediaQuery,
-} from '@chakra-ui/react'
+import { Box, Button, Flex, Link, Stack, useMediaQuery } from '@chakra-ui/react'
 
 import { RiHome4Fill } from 'react-icons/ri'
 
 const NavBar = () => {
-  const [isMobile] = useMediaQuery('(max-width: 768px)')
+  const [isMobile] = useMediaQuery('(max-width: 768px')
+
   return (
-    <>
-      <Box bg={'#f8f9eb'} height={90} pos="fixed" width="100%" px={4}>
-        <Flex
-          h={16}
-          alignItems={'center'}
-          justifyContent={'space-between'}
-          marginRight={100}
-        >
-          <Stack direction={'row'}>
-            <Center width="1690px">
-              <Link href="/">
-                <Box height="14px" width="400px">
-                  {isMobile ? (
-                    <>
-                      <RiHome4Fill />
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="100px"
-                        color="white"
-                        bg={'#1f1f1a'}
-                        _hover={{ border: '2px solid white' }}
-                      >
-                        Home
-                      </Button>
-                    </>
-                  )}
-                </Box>
-              </Link>
-              <Link href="/calender">
-                <Box height="14px" width="400px">
-                  {isMobile ? (
-                    <>
-                      <RiHome4Fill />
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="100px"
-                        color="white"
-                        bg="#1f1f1a"
-                        _hover={{ border: '2px solid white' }}
-                      >
-                        Schedules
-                      </Button>
-                    </>
-                  )}
-                </Box>
-              </Link>
-              <Link href="/settings">
-                <Box height="14px" width="400px">
-                  {isMobile ? (
-                    <>
-                      <RiHome4Fill />
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="100px"
-                        color="white"
-                        bg="#1f1f1a"
-                        _hover={{ border: '2px solid white' }}
-                      >
-                        Settings
-                      </Button>
-                    </>
-                  )}
-                </Box>
-              </Link>
-            </Center>
-          </Stack>
-        </Flex>
-      </Box>
-    </>
+    <Flex
+      h={90}
+      alignItems="center"
+      justifyContent="center" // Center-align buttons
+      bg={'transparent'}
+      pos="fixed"
+      top={0}
+      left={0}
+      right={0}
+      px={4}
+    >
+      <Stack direction="row" spacing={250}>
+        <Link href="/">
+          <Button
+            size="md"
+            height="48px"
+            width="100px"
+            color="white"
+            bg="#1f1f1a"
+            _hover={{ border: '2px solid white' }}
+          >
+            Home
+          </Button>
+        </Link>
+        <Link href="/schedules">
+          <Button
+            size="md"
+            height="48px"
+            width="100px"
+            color="white"
+            bg="#1f1f1a"
+            _hover={{ border: '2px solid white' }}
+          >
+            Schedules
+          </Button>
+        </Link>
+        <Link href="/settings">
+          <Button
+            size="md"
+            height="48px"
+            width="100px"
+            color="white"
+            bg="#1f1f1a"
+            _hover={{ border: '2px solid white' }}
+          >
+            Settings
+          </Button>
+        </Link>
+      </Stack>
+    </Flex>
   )
 }
 
