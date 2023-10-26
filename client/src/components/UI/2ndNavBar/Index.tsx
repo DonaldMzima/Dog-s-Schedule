ba7e2f8ffae216bfeafe9fb36040fe9b0eb8577e
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-10-26 17:21:18 +0200

    ref:fix navbar position

--- client/src/components/UI/2ndNavBar/Index.tsx
+++ client/src/components/UI/2ndNavBar/Index.tsx
@@ -16,42 +16,47 @@ const NavigationBar = () => {
   const router = useRouter() // Get the router object
 
   return (
-    <>
-      <Flex>
-        <Center>
-          <Stack
-            direction={'row'}
-            zIndex={1}
-            color="black"
-            spacing={{ base: 20, sm: 155 }}
-            ml={{ base: 25, sm: 50 }}
-            mt={{ base: -150, sm: -120 }}
+    <Box
+      position="fixed"
+      bottom="0"
+      left="0"
+      width="100%"
+      zIndex={1}
+      background="white" // Change this to the desired background color
+      boxShadow="lg"
+      paddingY="20px"
+    >
+      <Center>
+        <Stack
+          w="100%"
+          direction={'row'}
+          color="black"
+          spacing={{ base: 20, sm: 155 }}
+          ml={{ base: 25, sm: 50 }}
+          mt={{ base: -150, sm: -120 }}
+        >
+          <Link
+            href="/"
+            borderBottom={router.pathname === '/' ? '2px solid black' : 'none'}
           >
-            <Link
-              href="/"
-              borderBottom={
-                router.pathname === '/' ? '2px solid black' : 'none'
-              }
-            >
-              <Box mb={4}>
-                <RiHome4Fill size={'3em'} />
-              </Box>
-            </Link>
-            <Link
-              href="/schedules"
-              borderBottom={
-                router.pathname === '/schedules' ? '2px solid black' : 'none'
-              }
-            >
-              <Box mb={4}>
-                <BsCalendarPlus size={'3em'} />
-              </Box>
-            </Link>
-            <SettingDrawer />
-          </Stack>
-        </Center>
-      </Flex>
-    </>
+            <Box mb={4}>
+              <RiHome4Fill size={'3em'} />
+            </Box>
+          </Link>
+          <Link
+            href="/schedules"
+            borderBottom={
+              router.pathname === '/schedules' ? '2px solid black' : 'none'
+            }
+          >
+            <Box mb={4}>
+              <BsCalendarPlus size={'3em'} />
+            </Box>
+          </Link>
+          <SettingDrawer />
+        </Stack>
+      </Center>
+    </Box>
   )
 }
 
