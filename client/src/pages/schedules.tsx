db569690ddeeb1a20bd28d19e64165880bd65370
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-12-14 17:08:03 +0200

    ref: add signin in schedule router

--- client/src/pages/schedules.tsx
+++ client/src/pages/schedules.tsx
@@ -1,10 +1,16 @@
 import Schedules from '@/components/CalenderSection/Scheldules'
 import { Box } from '@chakra-ui/react'
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs'
 
 const Calender = () => {
   return (
     <Box>
-      <Schedules />
+      <SignedIn>
+        <Schedules />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </Box>
   )
 }
