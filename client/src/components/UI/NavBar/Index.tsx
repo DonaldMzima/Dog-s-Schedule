8370247aa780154f101f5c4282c8171f813920bd
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2024-03-12 14:03:54 +0200

    ref: replace buttons with icons

--- client/src/components/UI/NavBar/Index.tsx
+++ client/src/components/UI/NavBar/Index.tsx
@@ -1,25 +1,27 @@
+import { DarkMode } from '@/components/DarkMode'
 import SettingDrawer from '@/components/Settings'
 import {
   Box,
   Button,
   Center,
   Flex,
-  Image,
   Link,
   Stack,
-  useColorMode,
+  IconButton,
   useColorModeValue,
   useMediaQuery,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { SetStateAction, useState } from 'react'
+import { FiBell } from 'react-icons/fi'
+import FloatingButtons from '../Buttons/Index'
+import { RiHome4Fill } from 'react-icons/ri'
 
 const NavBar = () => {
   const [isMobile] = useMediaQuery('(max-width: 768px)')
   const [activeButton, setActiveButton] = useState('')
 
   const router = useRouter()
-  const { colorMode } = useColorMode()
 
   const handleButtonClick = (path: SetStateAction<string>) => {
     setActiveButton(path)
@@ -28,85 +30,46 @@ const NavBar = () => {
   return (
     <>
       <Box
-        h={90}
+        ml={{ base: 0, md: 60 }}
+        px={{ base: 4, md: 4 }}
+        height="20"
+        alignItems="center"
         bg={useColorModeValue('white', 'gray.900')}
+        borderBottomWidth="1px"
         pos="fixed"
         top={0}
         zIndex={1}
         left={0}
         right={0}
-        px={4}
-        boxShadow="md"
-        alignItems="center"
         justifyContent="center"
       >
         {/* Logo */}
-        {colorMode === 'light' && (
-          <Image
-            src="/dogwalkschedule.png"
-            alt="DogWalkSchedule Logo"
-            height="30px"
-            pos="absolute"
-            mr={{ base: 1000, sm: 1200, md: 1400, lg: 1400 }}
-            display="flex"
-            mt={29}
-          />
-        )}
-        <Center>
-          <Stack
-            direction="row"
-            spacing={250}
-            alignItems="center"
-            justify="center"
-            mt={17}
+
+        <Stack
+          direction="row"
+          spacing={31}
+          alignItems="center"
+          justify="flex-end"
+          mt={17}
+        >
+          <Link
+            href="/"
+            borderBottom={router.pathname === '/' ? '2px solid black' : 'none'}
           >
-            <Link href="/">
-              <Button
-                size="md"
-                height="48px"
-                width="100px"
-                border={useColorModeValue(
-                  '2px solid #1f1f1a',
-                  '1px solid #fafaf7',
-                )}
-                color={useColorModeValue(
-                  router.pathname === '/' ? '#1f1f1a' : 'white',
-                  router.pathname === '/' ? 'white' : 'gray.900',
-                )}
-                bg={useColorModeValue(
-                  router.pathname === '/' ? '#fafaf7' : '#1f1f1a',
-                  router.pathname === '/' ? 'gray.900' : '#fafaf7',
-                )}
-                onClick={() => handleButtonClick('/')}
-              >
-                Home
-              </Button>
-            </Link>
-            <Link href="/schedules">
-              <Button
-                size="md"
-                height="48px"
-                width="100px"
-                border={useColorModeValue(
-                  '2px solid #1f1f1a',
-                  '1px solid #fafaf7',
-                )}
-                color={useColorModeValue(
-                  router.pathname === '/schedules' ? '#1f1f1a' : 'white',
-                  router.pathname === '/schedules' ? 'white' : 'gray.900',
-                )}
-                bg={useColorModeValue(
-                  router.pathname === '/schedules' ? '#fafaf7' : '#1f1f1a',
-                  router.pathname === '/schedules' ? 'gray.900' : '#fafaf7',
-                )}
-                onClick={() => handleButtonClick('/schedules')}
-              >
-                Schedules
-              </Button>
-            </Link>
-            <SettingDrawer />
-          </Stack>
-        </Center>
+            <IconButton size="md" variant="ghost" aria-label="open menu">
+              <RiHome4Fill size={30} />
+            </IconButton>
+          </Link>
+
+          <IconButton
+            size="md"
+            variant="ghost"
+            aria-label="open menu"
+            icon={<FiBell size={30} />}
+          />
+
+          <DarkMode />
+        </Stack>
       </Box>
     </>
   )
--- client/src/pages/schedules.tsx
+++ client/src/pages/schedules.tsx
@@ -1,6 +1,4 @@
-import { ApolloProviderWrapper } from '@/components/Authentication/ApolloProviderWrapper'
 import Schedules from '@/components/CalenderSection/Scheldules'
-
 import { Box } from '@chakra-ui/react'
 import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs'
 
