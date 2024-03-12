c87a30dabe242c4f26ebd6ab2c2b0b7fd2644e7b
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2024-03-12 13:53:22 +0200

    fix: delete bunussed buttons

--- client/src/components/CalenderSection/Scheldules/Sidebar.tsx
+++ client/src/components/CalenderSection/Scheldules/Sidebar.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import {
-  Box,
   VStack,
   Text,
   IconButton,
   Icon,
   Button,
   useColorModeValue,
+  CloseButton,
+  Flex,
+  Image,
+  useColorMode,
+  Box,
 } from '@chakra-ui/react'
 import { FaUser, FaSignOutAlt, FaTimes, FaComment } from 'react-icons/fa'
 import { BiDotsVertical } from 'react-icons/bi'
@@ -17,6 +21,7 @@ import { showCompletedState, openFeedbackModal } from '@/store/atoms'
 import { UserButton, useClerk } from '@clerk/nextjs'
 import ProfileModal from '@/components/Modals/ProfileModal'
 import FeedbackModal from '@/components/Modals/FeedbackModal'
+import { FiMenu } from 'react-icons/fi'
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -30,6 +35,8 @@ const Sidebar = () => {
   const openProfileModal = () => {
     setIsProfileModalOpen(true)
   }
+
+  const { colorMode } = useColorMode()
 
   const closeProfileModal = () => {
     setIsProfileModalOpen(false)
@@ -50,85 +57,93 @@ const Sidebar = () => {
   return (
     <>
       <IconButton
-        icon={isOpen ? <FaTimes /> : <BiDotsVertical size={24} />}
+        icon={isOpen ? <FiMenu /> : <BiDotsVertical size={24} />}
         aria-label="Toggle Sidebar"
         display={{ base: 'block', md: 'none' }}
         onClick={toggleSidebar}
-        top="-70px"
+        top="0%"
         variant={'unstyled'}
         color={'black'}
       />
 
+      {/* <IconButton
+        display={{ base: 'flex', md: 'none' }}
+        onClick={onOpen}
+        variant="outline"
+        aria-label="open menu"
+        icon={<FiMenu />}
+      /> */}
+
       <Box
-        as="nav"
+        transition="1s ease"
+        borderRight="1px"
+        w={{ base: 'full', md: 60 }}
         pos="fixed"
+        h="full"
+        as="nav"
         left={0}
-        top={90}
         bottom={0}
-        w="200px"
         bg={useColorModeValue('white', 'gray.900')}
         color={useColorModeValue('#000000', 'white')}
         p={{ base: 2, md: 14 }}
         display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
-        zIndex={1}
         boxShadow="lg"
+        zIndex={1}
+        borderColor={useColorModeValue('gray.200', 'gray.700')}
       >
-        <VStack spacing={8} alignItems="center">
-          <UserButton afterSignOutUrl="/" />
+        <Box>
+          {colorMode === 'light' && (
+            <Image
+              mr={550}
+              src="/dogwalkschedule.png"
+              alt="DogWalkSchedule Logo"
+            />
+          )}
+        </Box>
 
-          <Text fontSize="xl" ml={0}>
-            _
-          </Text>
+        <VStack spacing={5} alignItems="center">
+          <UserButton afterSignOutUrl="/" />
         </VStack>
-        <VStack spacing={8} mr="50%">
-          <Button
-            leftIcon={<Icon as={FaUser} boxSize={5} />}
-            variant="ghost"
-            w="100%"
-            onClick={openProfileModal}
-          >
-            Profile
-          </Button>
-          <ProfileModal
-            isOpen={isProfileModalOpen}
-            onClose={closeProfileModal}
-          />
-          <Button
-            leftIcon={<Icon as={AiOutlineCheckSquare} boxSize={5} />}
-            variant="ghost"
-            pos="static"
-            onClick={Completedtasks}
-            w="100%"
-          >
-            {showCompleted ? (
-              <Text>Show All</Text>
-            ) : (
-              <Text fontSize="xs">Completed Schedules</Text>
-            )}
-          </Button>
-          <Button
-            leftIcon={<Icon as={FaComment} boxSize={5} />}
-            variant="ghost"
-            w="100%"
-            onClick={openFeedModal}
-          >
-            Feedback
-          </Button>
-          <FeedbackModal
-            isOpen={Feedback}
-            onClose={() => setFeedbackModal(false)}
-          />
-          <Button
-            leftIcon={<Icon as={FaSignOutAlt} boxSize={5} />}
-            variant="ghost"
-            w="100%"
-            mt="200%"
-            pos="absolute"
-            onClick={() => signOut(() => router.push('/'))}
-          >
-            Sign out
-          </Button>
-        </VStack>
+
+        <Box
+          as="nav"
+          style={{ textDecoration: 'none' }}
+          _focus={{ boxShadow: 'none' }}
+        >
+          <VStack p="4" borderRadius="lg" cursor="pointer">
+            <Button
+              leftIcon={<Icon as={AiOutlineCheckSquare} boxSize={5} />}
+              variant="ghost"
+              pos="static"
+              onClick={Completedtasks}
+            >
+              {showCompleted ? (
+                <Text>Show All</Text>
+              ) : (
+                <Text>Completed Schedules</Text>
+              )}
+            </Button>
+            <Button
+              leftIcon={<Icon as={FaComment} boxSize={5} />}
+              variant="ghost"
+              onClick={openFeedModal}
+            >
+              Feedback
+            </Button>
+            <FeedbackModal
+              isOpen={Feedback}
+              onClose={() => setFeedbackModal(false)}
+            />
+            <Button
+              leftIcon={<Icon as={FaSignOutAlt} boxSize={5} />}
+              variant="ghost"
+              mt="200%"
+              onClick={() => signOut(() => router.push('/'))}
+            >
+              Sign out
+            </Button>
+          </VStack>
+        </Box>
       </Box>
     </>
   )
